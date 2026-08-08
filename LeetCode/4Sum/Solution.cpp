e82384a03f12ce8d7faef214d10bf96d6c14d056
tejareddy1248 <tejareddy240407@gmail.com>
2026-08-08 22:00:44 +0530

    Add solution for 4Sum

---
+++ LeetCode/4Sum/Solution.cpp
@@ -0,0 +1,28 @@
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+      vector<vector<int>> v;
+      sort(nums.begin(), nums.end());
+      
+      for(int i = 0; i < nums.size(); i++){
+        if(i != 0 && nums[i] == nums[i - 1]) continue;
+        for(int j = i + 1; j < nums.size(); j++){
+          if(j != i + 1 && nums[j] == nums[j - 1]) continue;
+          
+          int k = j + 1;
+          int l = nums.size() - 1;
+          
+          while(k < l){
+            int sum = nums[i] + nums[j] + nums[k] + nums[l];
+            if(target - sum == 0){
+              v.push_back({nums[i], nums[j], nums[k], nums[l]});
+              k++;
+              while(k < l && nums[k] == nums[k - 1]) k++;
+            }
+            else if(sum < target) k++;
+            else l--;
+          }
+          
+        }
+      }
+      
+      return v;
+}
